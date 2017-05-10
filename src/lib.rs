fd6e04a20193b73ed383e2b53691e6985102bec6
Adrian Brink <adrian@brink-holdings.com>
2017-05-10 12:14:23 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+pub fn hello() -> String {
+    "Hello!".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+extern crate rust_abci;
+
+fn main() {
+    println!("{:?}", rust_abci::hello())
+}
